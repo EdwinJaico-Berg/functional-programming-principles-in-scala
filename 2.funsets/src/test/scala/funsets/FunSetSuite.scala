c2ae5abacc221a952a8ac7a355035bece6b3216a
Edwin Jaico-Berg <e.j.berg@outlook.com>
2023-04-05 14:23:45 +0100

    Completed assignment for week 2

--- 2.funsets/src/test/scala/funsets/FunSetSuite.scala
+++ 2.funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -36,6 +36,18 @@ class FunSetSuite extends munit.FunSuite:
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+    val s4 = singletonSet(4)
+    val s5 = singletonSet(5)
+    val s6 = singletonSet(6)
+    val univSet = union(s1, union(s2, union(s3, union(s4, union(s5, s6)))))
+
+    val completeSet = createSet(union, 1000)
+
+    def createSet(f: (FunSet, FunSet) => FunSet, bound: Int): FunSet =
+      def recur(a: Int): FunSet =
+        if a == bound - 1 then (f(singletonSet(a), singletonSet(a+1)))
+        else f(singletonSet(a), recur(a + 1))
+      recur(-bound)
 
   /**
    * This test is currently disabled (by using .ignore) because the method
@@ -45,7 +57,7 @@ class FunSetSuite extends munit.FunSuite:
    * .ignore annotation.
    */
 
-    test("singleton set one contains one".ignore) {
+  test("singleton set one contains one") {
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -67,7 +79,39 @@ class FunSetSuite extends munit.FunSuite:
       assert(!contains(s, 3), "Union 3")
   }
 
+  test("intersect contains the intersecting elements") {
+    new TestSets:
+      val s = union(s1, s2)
+      val t = union(s2, s3)
+      val i = intersect(s, t)
+      assert(contains(i, 2), "Intersection between {1, 2} {2, 3}")
+      assert(!contains(i, 1), "Intersection between {1, 2} {2, 3}")
+      assert(!contains(i, 3), "Intersection between {1, 2} {2, 3}")
+  }
 
+  test("diff contains the difference of two sets") {
+    new TestSets:
+      val t = union(s2, union(s4, s6))
+      val d = diff(univSet, t)
+      assert(contains(d, 1) && contains(d, 3) && contains(d, 5), "Difference contains {1, 3, 5}")
+  }
+
+  test("filter returns set containing even numbers") {
+    new TestSets:
+      val f = filter(univSet, x => x % 2 == 0)
+      assert(contains(f, 2) && contains(f, 4) && contains(f, 6), "Contains even numbers")
+  }
+
+  test("forall returns true for identity and false for square") {
+    new TestSets:
+      assert(forall(completeSet, x => x == x), "Identity")
+      assert(!forall(completeSet, x => x == x * x), "Square")
+  }
+
+  test("exists returns true for set that contains squares") {
+    new TestSets:
+      assert(exists(completeSet, x => x == x * x), "Square")
+  }
 
   import scala.concurrent.duration.*
   override val munitTimeout = 10.seconds
